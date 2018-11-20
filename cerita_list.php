249197cb05c87de984d4e7a7542c33232a9485ab
VaniaC <vaniac@users.noreply.github.com>
2018-11-21 00:51:23 +0700

    test

--- cerita_list.php
+++ cerita_list.php
@@ -1,7 +1,16 @@
 <?php  
 session_start();
 include "db_connection.php";
-$query
+
+$query = "SELECT * FROM cerita";
+$result = mysql_query($query);
+
+<?php
+
+while ($data = mysql_fetch_array($result)) {
+	echo '<img src=".$data[ikon]." />';
+	echo ".$data[judul].";
+}
 
 
 
